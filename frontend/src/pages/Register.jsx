21e779c238af84f7c3a5d29b4dd56ff9b0f8cba0
sowrya647 <chandrasowrya192@gmail.com>
2025-10-07 10:35:37 +0530

    Update Register.jsx

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -9,7 +9,7 @@ function Register() {
 
   const handleRegister = async () => {
     try {
-      const response = await fetch("http://localhost:5000/register", {
+      const response = await fetch("https://my-auth-app-v7lr.onrender.com/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, password }),
